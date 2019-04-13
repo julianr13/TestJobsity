d3dd4d8c3ef5f1e5101793a2d5c4f71fdf314eaf
Julian Rosales <julianr13@hotmail.com>
2019-04-13 15:11:21 -0500

    instlador

---
+++ Chatroom/UnitTestChat/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chatroom;
+namespace UnitTestChat
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_LoginMethod()
+        {
+            Usuarios  usuarios = new Usuarios();
+            string res = usuarios.Login("julian","12345123");
+            Assert.AreEqual(res, "julian");
+        }
+    }
+}
